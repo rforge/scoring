160dc331bebb5b9daeb6ceb165e20039c81095bf
ecmerkle <ecmerkle@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-18 19:10:57 +0000

    Added test code run through package testthat.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/scoring@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/tests/test-02-multi.R
@@ -0,0 +1,32 @@
+context('multicategory')
+
+test_that('manyalt', {
+  data(WeatherProbs)
+  scores <- calcscore(tcat ~ tblw + tnrm + tabv, fam="pow",
+                      param=c(2,rep(1/3,3)), data=WeatherProbs,
+                      bounds=c(0,1))
+  expect_is(scores, 'numeric')
+  expect_false(any(is.na(scores)))
+  expect_true(all(scores <= 1) & all(scores >= 0))
+
+  scores2 <- calcscore(tcat ~ tblw + tnrm + tabv, fam="sph",
+                       param=c(2,rep(1/3,3)), data=WeatherProbs,
+                       bounds=c(0,1), reverse=TRUE)
+  expect_is(scores2, 'numeric')
+  expect_false(any(is.na(scores2)))
+  expect_true(all(scores2 <= 1) & all(scores2 >= 0))
+
+  scores3 <- calcscore(tcat ~ tblw + tnrm + tabv, fam="sph",
+                       param=c(3,.1,.1,.8), data=WeatherProbs,
+                       bounds=c(0,1), reverse=TRUE)
+  expect_is(scores3, 'numeric')
+  expect_false(any(is.na(scores3)))
+  expect_true(all(scores3 <= 1) & all(scores3 >= 0))
+
+  scores4 <- calcscore(tcat ~ tblw + tnrm + tabv, fam="sph",
+                       param=c(3,.1,.1,.8), data=WeatherProbs[1:500,],
+                       bounds=c(0,1), reverse=TRUE, ordered=TRUE)
+  expect_is(scores4, 'numeric')
+  expect_false(any(is.na(scores4)))
+  expect_true(all(scores4 <= 1) & all(scores4 >= 0))
+})
